a4f6cf3a96559622d9106136398c5c9f08abafef
Jayson Alanano <json.alanano@gmail.com>
2024-05-22 22:41:36 +0800

    added header, side navigation and parsers

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-row justify-center p-5">
+  <div>
+    <img src="{{ asset('images/Logo-1.png') }}" alt="logo" class="w-20 h-20 mx-5" />
+  </div>
+  <div>
+    <div class="text-center">
+      <h1 class="text-xl font-bold">Department of Orthopaedics</h1>
+      <h2 class="text-md">DAVAO REGIONAL MEDICAL CENTER</h2>
+      <p class="text-md">Tagum City, Davao del Norte, PHILIPPINES</p>
+    </div>
+  </div>
+  <div>
+    <img src="{{ asset('images/Logo-2.jpg') }}" alt="logo" class="w-20 h-20 mx-5" />
+  </div>
+</div>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+  {{-- header --}}
+  <x-header></x-header>
+
+  {{-- side navigation --}}
+  <x-side-navigation></x-side-navigation>
+@endsection
