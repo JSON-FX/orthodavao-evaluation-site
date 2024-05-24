26975d1f7c9918dbb6df1d1c467aacb6b72aa969
Jayson Alanano <json.alanano@gmail.com>
2024-05-25 00:34:45 +0800

    revised header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,23 @@
 <div class="flex flex-row justify-center p-5">
   <div>
-    <img src="{{ asset('images/Logo-1.png') }}" alt="logo" class="w-32 h-32 mx-5" />
+    <img
+      src="{{ asset('images/Logo-1.png') }}"
+      alt="logo"
+      class="mx-auto w-16 h-16 sm:w-32 sm:h-32 sm:mx-5"
+    />
   </div>
   <div>
-    <div class="text-center">
-      <h1 class="text-4xl font-bold">Department of Orthopaedics</h1>
-      <h2 class="text-xl">DAVAO REGIONAL MEDICAL CENTER</h2>
-      <p class="text-xl">Tagum City, Davao del Norte, PHILIPPINES</p>
+    <div class="text-center mt-2 sm:text-center">
+      <h1 class="font-bold sm:text-4xl">Department of Orthopaedics</h1>
+      <h2 class="text-xs sm:text-xl">DAVAO REGIONAL MEDICAL CENTER</h2>
+      <p class="text-xs sm:text-xl">Tagum City, Davao del Norte, PHILIPPINES</p>
     </div>
   </div>
   <div>
-    <img src="{{ asset('images/Logo-2.jpg') }}" alt="logo" class="w-32 h-32 mx-5" />
+    <img
+      src="{{ asset('images/Logo-2.jpg') }}"
+      alt="logo"
+      class="mx-auto w-16 h-16 sm:w-32 sm:h-32 sm:mx-5"
+    />
   </div>
 </div>
